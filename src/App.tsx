0abfef42657dfb0d37780cfd401dcc535bc44af5
koharu <koharu.8782s@gmail.com>
2025-12-26 12:18:00 +0900

    CSS整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import "@/styles/sanitize.css";
+import "@/styles/global.css";
+import "@/styles/layout.css";
+import "@/styles/tabs.css";
+import "@/styles/card.css";
+import "@/styles/weatherDetails.css";
 import { WeatherOutfitContainer } from "@/containers/WeatherOutfitContainer";
 
 function App() {
